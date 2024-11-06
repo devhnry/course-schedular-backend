499af4618a63cbafdd855dc84eb2a5aafdf5fbd5
devhnry <devwhenry@gmail.com>
2024-11-06 20:32:21 +0100

    build: project bootstrap

---
+++ src/test/java/com/henry/universitycourseschedular/UniversitycourseschedularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.universitycourseschedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitycourseschedularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
