2d885667abce3f3d9cc2913edb64b407dabc1991
devhnry <devthenry@gmail.com>
2024-11-06 20:32:21 +0100

    build: project bootstrap

---
+++ src/main/java/com/henry/universitycourseschedular/UniversitycourseschedularApplication.java
@@ -0,0 +1,13 @@
+package com.henry.universitycourseschedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitycourseschedularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversitycourseschedularApplication.class, args);
+    }
+
+}
