b302ae381686130f50e1740b228d28d7d88d77e7
devhnry <devthenry@gmail.com>
2025-01-22 12:35:53 +0100

    feat: Create AuthToken entity to store JWT tokens and their status

---
+++ src/main/java/com/henry/universitycourseschedular/entity/AuthToken.java
@@ -0,0 +1,37 @@
+package com.henry.universitycourseschedular.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String tokenId;
+
+    @Column(nullable = false)
+    private String accessToken;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private Boolean expiredOrRevoked;
+
+    @Column(nullable = false)
+    private Instant expiresAt;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "userId", referencedColumnName = "userId")
+    private BaseUser user;
+}
