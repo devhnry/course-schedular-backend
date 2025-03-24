f9a1126eaace022a69ae7573c42b29ad5e5cac1b
devhnry <devthenry@gmail.com>
2025-03-24 11:37:36 +0100

    feat: Add Lecturer entity with one-to-many Courses and many-to-many available TimeSlots

---
+++ src/main/java/com/henry/universitycourseschedular/entity/Lecturer.java
@@ -0,0 +1,31 @@
+package com.henry.universitycourseschedular.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Lecturer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long lectureId;
+
+    private String name;
+
+    // One lecturer can have many courses.
+    @OneToMany(mappedBy = "lecturer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Course> courses;
+
+    // Many lecturers may have the same available timeslot and vice versa.
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "lecturer_timeslots",
+            joinColumns = @JoinColumn(name = "lecturer_id"),
+            inverseJoinColumns = @JoinColumn(name = "timeslot_id"))
+    private Set<TimeSlot> availableSlots;
+}
