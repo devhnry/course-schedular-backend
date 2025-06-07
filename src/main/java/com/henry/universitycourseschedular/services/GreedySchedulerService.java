732b1cb51d88fe13e001428a785337d399102833
devhnry <devthenry@gmail.com>
2025-06-07 11:50:01 +0100

    feat(scheduling): defined GreedySchedulerService interface

---
+++ src/main/java/com/henry/universitycourseschedular/services/GreedySchedulerService.java
@@ -0,0 +1,13 @@
+package com.henry.universitycourseschedular.services;
+
+import com.henry.universitycourseschedular.models.core.Venue;
+import com.henry.universitycourseschedular.models.course.CourseAssignment;
+import com.henry.universitycourseschedular.models.schedule.ScheduleEntry;
+import com.henry.universitycourseschedular.models.schedule.TimeSlot;
+
+import java.util.List;
+
+public interface GreedySchedulerService {
+    List<ScheduleEntry> assignCourses(List<CourseAssignment> assignments, List<TimeSlot> slots, List<Venue> venues);
+}
+
