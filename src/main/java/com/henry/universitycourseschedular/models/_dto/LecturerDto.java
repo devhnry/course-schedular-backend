3808a464090a3cf2f742ce4c2b4e8b5d57a5e223
devhnry <devthenry@gmail.com>
2025-06-09 09:09:36 +0100

    refactor(core): convert LecturerDto to Java record and update mapper accordingly

--- src/main/java/com/henry/universitycourseschedular/models/_dto/LecturerDto.java
+++ src/main/java/com/henry/universitycourseschedular/models/_dto/LecturerDto.java
@@ -1,24 +1,13 @@
 package com.henry.universitycourseschedular.models._dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.henry.universitycourseschedular.enums.Title;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
 
-@Data
-@JsonIgnoreProperties
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class LecturerDto {
-    @NotBlank(message = "First name is Required")
-    private String firstName;
-
-    @NotBlank(message = "Last name is Required")
-    private String lastName;
-
-    @NotBlank(message = "Title is Required")
-    @Enumerated(EnumType.STRING)
-    private Title title;
-}
+public record LecturerDto(
+        @NotBlank String firstName,
+        @NotBlank String lastName,
+        @NotBlank @Enumerated(EnumType.STRING) Title title,
+        String email
+) {}
