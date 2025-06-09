20be22064f86649e059a5d8c63ca37c289613264
devhnry <devthenry@gmail.com>
2025-06-09 22:55:24 +0100

    feat(domain): add GeneralBody entity for managing general courses

--- src/main/java/com/henry/universitycourseschedular/models/course/Course.java
+++ src/main/java/com/henry/universitycourseschedular/models/course/Course.java
@@ -1,6 +1,6 @@
 package com.henry.universitycourseschedular.models.course;
 
-import com.henry.universitycourseschedular.enums.CourseType;
+import com.henry.universitycourseschedular.models.core.GeneralBody;
 import com.henry.universitycourseschedular.models.core.Program;
 import jakarta.persistence.*;
 import lombok.*;
@@ -28,8 +28,8 @@ public class Course {
     @ManyToOne(fetch = FetchType.LAZY)
     private Program program;
 
-    private int expectedStudents;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private GeneralBody generalBody;
 
-    @Enumerated(EnumType.STRING)
-    private CourseType courseType;
+    private int expectedStudents;
 }
